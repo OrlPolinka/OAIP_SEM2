143ebc74c21d353d64d05b80a5d71d0f46ad3d2c
OrlPolinka <153023767+OrlPolinka@users.noreply.github.com>
2024-10-22 00:23:21 +0300

    Add files via upload

---
+++ лр_4/lab4 dop1/lab4 dop1/lab4 dop1.cpp
@@ -0,0 +1,84 @@
+﻿// СПЕРВА НУЖНО ПРОЧЕСТЬ ФАЙЛ(3), ЗАТЕМ ВЫПОЛНЯТЬ ОСТАЛЬНЫЕ ДЕЙСТВИЯ
+
+#include <iostream>
+#include <windows.h>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+using namespace std;
+
+struct Student {
+    string name;
+    int maths;
+    int physics;
+    int english;
+    int oap;
+};
+
+void exams(Student* students, int SIZE) {
+
+    int numStudents = 0;
+    for (int i = 0; i < SIZE; ++i) {
+
+        if ((students[i].maths == 5 || students[i].maths == 4) &&
+            (students[i].physics == 5 || students[i].physics == 4) &&
+            (students[i].english == 5 || students[i].english == 4) &&
+            (students[i].oap == 5 || students[i].oap == 4))
+        {
+            cout << i << ") Студент: " << students[i].name << " сдал экзамены на 4 или 5" << endl;
+            numStudents++;
+        }
+        else cout << i << ") Студент: " << students[i].name << " не сдал экзамены на 4 или 5" << endl;
+    }
+    cout << "Сотношение студентов, сдавших экзамен на 4 или 5, ко всем студентам " << ((double)numStudents / (double)SIZE) * 100 << " % " << endl;
+}
+
+void displayInfo(Student* students, int SIZE) {
+    for (int i = 0; i < SIZE; ++i) {
+        cout << "Имя студента: " << students[i].name << endl;
+        cout << "Математика: " << students[i].maths << endl;
+        cout << "Физика: " << students[i].physics << endl;
+        cout << "Английский: " << students[i].english << endl;
+        cout << "ОАП: " << students[i].oap << endl;
+    }
+}
+
+void  ifileStud(Student* students, int SIZE) {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    ifstream fin("file.txt");
+    if (!fin.is_open()) {
+        cout << "Не удаётся открыть файл для чтения " << endl;
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < SIZE; ++i) {
+        fin >> students[i].maths;
+        fin >> students[i].physics;
+        fin >> students[i].english;
+        fin >> students[i].oap;
+        fin >> students[i].name;
+    }
+    cout << "\nСчитана информация из файла\n";
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, ".1251");
+    const int SIZE = 10;
+    Student students[SIZE];
+    short choose;
+    do {
+        cout << "\nВыберите, что сделать:\n1 — проверить, сколько студентов положительно сдали экзамены;\n2 — вывести в консоль информацию о студенте;\n3 — считать информацию из файла;\n4 — выход из программы.\n";
+        cin >> choose;
+        switch (choose)
+        {
+        case 1: exams(students, SIZE); break;
+        case 2: displayInfo(students, SIZE); break;
+        case 3: ifileStud(students, SIZE); break;
+        case 4: exit(0); break;
+        }
+    } while (choose != 4);
+}
---
+++ лр_4/lab4 dop2/lab4 dop2/lab4 dop2.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <windows.h>
+
+
+using namespace std;
+
+struct TRAIN {
+    string destination;
+    int trainNumber;   
+    string departureTime; 
+};
+
+// Функция для сравнения по пункту назначения для сортировки
+bool compareDestinations(const TRAIN& train1, const TRAIN& train2) {
+    return train1.destination < train2.destination;
+}
+
+void printTrains(const TRAIN trains[], int size, const string& time) {
+    bool found = false;
+    cout << "Поезда, отправляющиеся после времени " << time << ":\n";
+    for (int i = 0; i < size; ++i) {
+        if (trains[i].departureTime > time) {
+            cout << "Пункт назначения: " << trains[i].destination << endl;
+            cout << "Номер поезда: " << trains[i].trainNumber << endl;
+            cout << "Время отправления: " << trains[i].departureTime << endl << endl;
+            found = true;
+        }
+    }
+    if (!found) {
+        cout << "Поездов, отправляющихся после указанного времени, нет.\n";
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    setlocale(LC_ALL, "ru");
+    const int SIZE = 8; 
+
+    TRAIN trains[SIZE];
+
+    cout << "Введите данные о поездах:\n";
+    for (int i = 0; i < SIZE; ++i) {
+        cout << "Поезд " << i + 1 << ":\n";
+        cout << "Пункт назначения: ";
+        cin >> trains[i].destination;
+        cout << "Номер поезда: ";
+        cin >> trains[i].trainNumber;
+        cout << "Время отправления (в формате HH:MM): ";
+        cin >> trains[i].departureTime;
+        cout << endl;
+    }
+
+    // Сортировка массива по пунктам назначения
+    sort(trains, trains + SIZE, compareDestinations);
+
+    string searchTime;
+    cout << "Введите время, после которого хотите найти поезда (в формате HH:MM): ";
+    cin >> searchTime;
+
+    printTrains(trains, SIZE, searchTime);
+
+    return 0;
+}
