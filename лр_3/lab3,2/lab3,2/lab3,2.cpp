e8197689956ab384bd6b940258887af2702577de
OrlPolinka <153023767+OrlPolinka@users.noreply.github.com>
2024-10-22 00:22:04 +0300

    Add files via upload

---
+++ лр_3/lab3,2/lab3,2/lab3,2.cpp
@@ -0,0 +1,62 @@
+﻿//Вариант 10 Задание 2
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+bool alphaP(string& word) {
+    for (char c : word) {
+        if (c == 'p' || c == 'P') {
+            return  true;
+        }
+    }
+    return false;
+}
+
+void inFile(ifstream& f, char s[100]) {
+    f.open(s);
+    if (f.fail()) {
+        cout << "\n Ошибка открытия файла";
+        exit(1);
+    }
+
+    string word;
+    while (f >> word) { 
+        if (alphaP(word)) { 
+            cout << word << endl; 
+        }
+    }
+
+    f.close();
+}
+
+void fromFile(ofstream& f, char s[100]) {
+    f.open(s);
+    if (f.fail()) {
+        cout << "\n Ошибка открытия файла";
+        exit(1);
+    }
+    cout << "Введите строку: " << endl;
+    cin.ignore(); // Очистим буфер ввода перед считыванием строки
+    cin.getline(s, 100); // Записываем строку в файл
+    f << s;
+    f.close();
+}
+
+int main() {
+    setlocale(LC_CTYPE, "RU");
+    char str[100];
+    ifstream ifile; 
+    ofstream ofile; 
+
+    cout << "\n Ввести имя файла для записи: \n";
+    cin >> str;
+    fromFile(ofile, str);
+    cout << endl;
+
+    cout << "\n Ввести имя файла для чтения: \n";
+    cin >> str;
+    inFile(ifile, str);
+
+    return 0;
+}
